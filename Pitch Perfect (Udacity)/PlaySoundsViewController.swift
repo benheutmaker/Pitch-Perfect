6e8bc06770c02a6ea4944c92239ccb26f9812f62
Benji <ben.heutmaker@icloud.com>
2015-03-17 22:50:20 -0700

    Comments and Refactoring

--- Pitch Perfect (Udacity)/PlaySoundsViewController.swift
+++ Pitch Perfect (Udacity)/PlaySoundsViewController.swift
@@ -5,11 +5,15 @@
 //  Created by Benji on 3/13/15.
 //  Copyright (c) 2015 Ben Heutmaker. All rights reserved.
 //
+//  This Controller is designed to display a set of buttons, and play the audio recorded on RecordAudioViewController with
+//  varying effects.
 
 import UIKit
 import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+    
+    //MARK: - Declarations
     
     var audioPlayer: AVAudioPlayer!
     var recievedAudio: RecordedAudio!
@@ -17,81 +21,146 @@ class PlaySoundsViewController: UIViewController {
     var audioEngine: AVAudioEngine!
     var audioFile: AVAudioFile!
     
+    
+    //MARK: - View Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Set up audioEngine with file recieved from RecordSoundViewController
         audioEngine = AVAudioEngine()
         audioFile = AVAudioFile(forReading: recievedAudio.filePath, error: nil)
         
+        //Set up audioPlayer with file recieved from RecordSoundViewController
         audioPlayer = AVAudioPlayer(contentsOfURL: recievedAudio.filePath, error: nil)
         audioPlayer.enableRate = true
     }
     
-    func playAudio(speed: Float) {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    //MARK: - Action Outlets
+    
+    //Each connected to corresponding buttons on IB
+    
+    @IBAction func slowAudioAction(sender: UIButton) {
+        playAudioWithVariablePitch(0.5)
+    }
+    
+    @IBAction func fastAudioAction(sender: UIButton) {
+        playAudioWithVariablePitch(2.0)
+    }
+    
+    @IBAction func chipmunkAudioAction(sender: UIButton) {
+        playAudioWithVariablePitch(2000)
+    }
+    
+    @IBAction func darthVaderAudioAction(sender: UIButton) {
+        playAudioWithVariablePitch(-2000)
+    }
+    
+    @IBAction func delayAudioAction(sender: UIButton) {
+        var interval = NSTimeInterval()
+        interval = 0.3
+        playAudioWithVariableDelay(interval)
+    }
+    
+    @IBAction func reverbAudioAction(sender: UIButton) {
+        playAudioWithVariableReverb(75)
+    }
+    
+    @IBAction func stopAction(sender: UIButton) {
+        stopPlayback()
+    }
+    
+    
+    //MARK: - Audio Effect Functions
+    
+    //Reusable func to stop all playback.
+    func stopPlayback() {
         audioPlayer.stop()
         audioEngine.stop()
         audioEngine.reset()
-        
+    }
+    
+    func playAudioWithVariableSpeed(speed: Float) {
+        stopPlayback()
         audioPlayer.rate = speed
         audioPlayer.currentTime = 0.0
         audioPlayer.play()
     }
-
-    @IBAction func playSlowAudio(sender: UIButton) {
-        playAudio(0.5)
-    }
     
-    @IBAction func playFastAudio(sender: UIButton) {
-        playAudio(2.0)
-    }
-    
-    @IBAction func playChipmunkAudio(sender: UIButton) {
-        playAudioWithVariablePitch(1000)
-    }
-    
-    @IBAction func playDarthVaderAudio(sender: UIButton) {
-        playAudioWithVariablePitch(-1000)
-    }
-    
+    //Change pitch of audio playing back.
     func playAudioWithVariablePitch(pitch: Float) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        stopPlayback()
         
+        //Set up a Node to hold the audio file to be played and point it at the engine.
         var audioPlayerNode = AVAudioPlayerNode()
         audioEngine.attachNode(audioPlayerNode)
         
+        //Set up pitch morphing node to change pitch and point it at the engine.
         var pitchVariable = AVAudioUnitTimePitch()
         pitchVariable.pitch = pitch
         audioEngine.attachNode(pitchVariable)
         
+        //Connect the audio player, to the effect, to the output for sound to play.
         audioEngine.connect(audioPlayerNode, to: pitchVariable, format: nil)
         audioEngine.connect(pitchVariable, to: audioEngine.outputNode, format: nil)
         
+        //Apply the audio to the player node and play the file.
         audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
         audioEngine.startAndReturnError(nil)
         
         audioPlayerNode.play()
     }
     
-    @IBAction func stopPlayback(sender: UIButton) {
-        audioPlayer.stop()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func playAudioWithVariableDelay(delay: NSTimeInterval) {
+        stopPlayback()
+        
+        //Set up a Node to hold the audio file to be played and point it at the engine.
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        //Set up delay effect node to add delay to audio, and point it at the engine.
+        var delayVariable = AVAudioUnitDelay()
+        delayVariable.delayTime = delay
+        audioEngine.attachNode(delayVariable)
+        
+        //Connect the audio player, to the effect, to the output for sound to play.
+        audioEngine.connect(audioPlayerNode, to: delayVariable, format: nil)
+        audioEngine.connect(delayVariable, to: audioEngine.outputNode, format: nil)
+        
+        //Apply the audio to the player node and play the file.
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func playAudioWithVariableReverb(reverb: Float) {
+        stopPlayback()
+        
+        //Set up a Node to hold the audio file to be played and point it at the engine.
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        //Set up reverb effect node to add reverb to audio, and point it at the engine.
+        var reverbVariable = AVAudioUnitReverb()
+        reverbVariable.wetDryMix = reverb
+        audioEngine.attachNode(reverbVariable)
+        
+        //Connect the audio player, to the effect, to the output for sound to play.
+        audioEngine.connect(audioPlayerNode, to: reverbVariable, format: nil)
+        audioEngine.connect(reverbVariable, to: audioEngine.outputNode, format: nil)
+        
+        //Apply the audio to the player node and play the file.
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
     }
-    */
 
 }
--- Pitch Perfect (Udacity)/RecordSoundsViewController.swift
+++ Pitch Perfect (Udacity)/RecordSoundsViewController.swift
@@ -6,18 +6,26 @@
 //  Copyright (c) 2015 Ben Heutmaker. All rights reserved.
 //
 
+
 import UIKit
 import AVFoundation
 
 class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
-
+    
+    //MARK: - Outlets
+    
     @IBOutlet weak var instructLabel: UILabel!
     @IBOutlet weak var stopRecordingButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playPauseButton: UIButton!
     
+    //MARK: - Variables
+    var isRecording: Bool!
     var audioRecorder:AVAudioRecorder!
     var recordedAudio: RecordedAudio!
+    
+    
+    //MARK: - View Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,9 +35,13 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
+        
+        //Set up UI
         instructLabel.text = "Tap to Record"
         stopRecordingButton.hidden = true
         playPauseButton.hidden = true
+        
+        isRecording = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,30 +49,21 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
-        if flag {
-            recordedAudio = RecordedAudio(title: recorder.url.lastPathComponent!, filePath: recorder.url)
-            
-            self.performSegueWithIdentifier("stopRecording", sender: recordedAudio)
-        } else {
-            println("Recording was not successful")
-            recordButton.enabled = true
-            stopRecordingButton.hidden = true
-        }
-    }
+    
+    //MARK: -
 
     @IBAction func recordAction(sender: UIButton) {
         //Show text "Recording in Progress"
         instructLabel.text = "Recording in Progress"
         
-        //Show stopRecordingButton
+        //Show stopRecordingButton and playPauseButton
         stopRecordingButton.hidden = false
+        playPauseButton.hidden = false
         
-        //TODO: Record the user's voice
-        println("in recordAudio")
-        
+        //Create a path to record the audio to.
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
         
+        //Create a date object to name the audiofile, and assign that name to the audio file array.
         let currentDateTime = NSDate()
         let formatter = NSDateFormatter()
         formatter.dateFormat = "ddMMyyyy-HHmmss"
@@ -69,23 +72,64 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         let filePath = NSURL.fileURLWithPathComponents(pathArray)
         println(filePath)
         
+        //Set up the audio session
         var session = AVAudioSession.sharedInstance()
         session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
         
+        //Set up audioRecorder
         audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
         audioRecorder.delegate = self
         audioRecorder.meteringEnabled = true
+        
+        //Record Audio
         audioRecorder.prepareToRecord()
         audioRecorder.record()
+        
+        isRecording = true
     }
     
+    //If audioRecorder isRecording, change the button to the play button and pause the recording. Else, do the opposite.
+    @IBAction func playPauseAction(sender: UIButton) {
+        if isRecording == true {
+            sender.setImage(UIImage(named: "play"), forState: .Normal)
+            audioRecorder.pause()
+            isRecording = false
+        } else if isRecording == false {
+            sender.setImage(UIImage(named: "pause"), forState: .Normal)
+            audioRecorder.record()
+            isRecording = true
+        }
+    }
+    
+    //Stops recorder, sets audioSession to false.
     @IBAction func stopAction(sender: UIButton) {
-        
-        //TODO: Stop recording the user's voice
         audioRecorder.stop()
         var audioSession = AVAudioSession.sharedInstance()
         audioSession.setActive(false, error: nil)
+        
+        isRecording = false
     }
+    
+    
+    //MARK: - Audio Recorder Flag
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
+        
+        //If recording was successful, create a filePath and perform segue to Play screen. Else, reset all UI.
+        if flag {
+            recordedAudio = RecordedAudio(title: recorder.url.lastPathComponent!, filePath: recorder.url)
+            self.performSegueWithIdentifier("stopRecording", sender: recordedAudio)
+            isRecording = false
+        } else {
+            println("Recording was not successful")
+            recordButton.enabled = true
+            stopRecordingButton.hidden = true
+            isRecording = false
+        }
+    }
+    
+    
+    //MARK: - Navigation
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "stopRecording" {
